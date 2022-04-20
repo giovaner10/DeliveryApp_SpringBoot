444806f6a53ab60c42018c2ac6810da6161bef07
giovaner10 <giovanerr10@gmail.com>
2022-04-20 19:28:33 -0300

    CozinhaService

--- src/main/java/com/app/deliveryapp/domain/service/CozinhaService.java
+++ src/main/java/com/app/deliveryapp/domain/service/CozinhaService.java
@@ -1,12 +1,13 @@
 package com.app.deliveryapp.domain.service;
 
+import com.app.deliveryapp.domain.exceptionhandler.EntidadeNaoEncontradaException;
 import com.app.deliveryapp.domain.repository.CozinhaRepository;
-import com.app.deliveryapp.domain.exception.EntidadeEmUsoException;
-import com.app.deliveryapp.domain.exception.EntidadeNaoEcontradaException;
 import com.app.deliveryapp.domain.model.Cozinha;
 import lombok.AllArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @AllArgsConstructor
@@ -14,22 +15,31 @@ public class CozinhaService {
 
     private CozinhaRepository cozinhaRepository;
 
+    public static final String NÃO_ECONTRADO = "NÃO ECONTRADO";
+
+
+    public Cozinha buscarId(Long id){
+        return cozinhaRepository.findById(id)
+                .orElseThrow(()-> new EntidadeNaoEncontradaException(NÃO_ECONTRADO));
+    }
+
     public Cozinha salvar(Cozinha cozinha){
         return cozinhaRepository.save(cozinha);
     }
 
     public void deletarCozinha(Long id){
 
-        if(!cozinhaRepository.existsById(id)){
-            throw new EntidadeNaoEcontradaException("Esse cozinha nao foi localizada no seu banco");
-        }
+        buscarId(id);
 
         try {
             cozinhaRepository.deleteById(id);
         }
         catch (DataIntegrityViolationException E){
-            throw new   EntidadeEmUsoException("Não foi possivel pois esse item se encontra associado " +
-                    "a outra tabela do seu banco");
+
+            throw  new ResponseStatusException(HttpStatus.CONFLICT, "ID associado a outra tabela.");
+
+            //throw new   EntidadeEmUsoException("Não foi possivel pois esse item se encontra associado " +
+                  //  "a outra tabela do seu banco");
         }
     }
 }
