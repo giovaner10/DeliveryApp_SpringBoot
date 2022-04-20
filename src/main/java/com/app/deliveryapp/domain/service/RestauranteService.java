725ad9da87def18cc76b9fffd50465010e6ad43a
giovaner10 <giovanerr10@gmail.com>
2022-04-20 19:27:39 -0300

    RestauranteService.java

--- src/main/java/com/app/deliveryapp/domain/service/RestauranteService.java
+++ src/main/java/com/app/deliveryapp/domain/service/RestauranteService.java
@@ -1,8 +1,7 @@
 package com.app.deliveryapp.domain.service;
 
+import com.app.deliveryapp.domain.exceptionhandler.EntidadeNaoEncontradaException;
 import com.app.deliveryapp.domain.repository.RestauranteRepository;
-import com.app.deliveryapp.domain.exception.EntidadeEmUsoException;
-import com.app.deliveryapp.domain.exception.EntidadeNaoEcontradaException;
 import com.app.deliveryapp.domain.model.Restaurante;
 import lombok.AllArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -14,21 +13,26 @@ public class RestauranteService {
 
     private RestauranteRepository restauranteRepository;
 
+    public static final String NÃO_ECONTRADO = "NÃO ECONTRADO";
+
+    public Restaurante buscarId(Long id){
+        return restauranteRepository.findById(id)
+                .orElseThrow(()-> new EntidadeNaoEncontradaException(NÃO_ECONTRADO));
+    }
+
     public Restaurante salvar(Restaurante cozinha){
         return restauranteRepository.save(cozinha);
     }
 
     public void deletaRestaurante(Long id){
 
-        if(!restauranteRepository.existsById(id)){
-            throw new EntidadeNaoEcontradaException("Esse restaurante nao foi localizada no seu banco de dados");
-        }
+        buscarId(id);
 
         try {
             restauranteRepository.deleteById(id);
         }
         catch (DataIntegrityViolationException E){
-            throw new   EntidadeEmUsoException("Não foi possivel pois esse item se encontra associado " +
+            throw new EntidadeNaoEncontradaException("Não foi possivel pois esse item se encontra associado " +
                     "a outra tabela do seu banco de dados");
         }
     }
