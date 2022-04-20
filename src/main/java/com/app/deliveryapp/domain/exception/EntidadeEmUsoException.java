dfbbf4de90e388fb8ddaea2a3593d13ad627167b
giovaner10 <giovanerr10@gmail.com>
2022-04-20 19:27:14 -0300

    adicao da exceptions

--- src/main/java/com/app/deliveryapp/domain/exception/EntidadeEmUsoException.java
+++
@@ -1,8 +0,0 @@
-package com.app.deliveryapp.domain.exception;
-
-public class EntidadeEmUsoException extends RuntimeException{
-
-    public EntidadeEmUsoException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/app/deliveryapp/domain/exception/EntidadeNaoEcontradaException.java
+++
@@ -1,8 +0,0 @@
-package com.app.deliveryapp.domain.exception;
-
-public class EntidadeNaoEcontradaException extends RuntimeException{
-
-    public EntidadeNaoEcontradaException(String message) {
-        super(message);
-    }
-}
