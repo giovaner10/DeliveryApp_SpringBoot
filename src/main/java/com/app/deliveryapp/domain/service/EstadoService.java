811ef1fa614d3319588a3ff9cb55ae8afcf38c1e
giovaner10 <giovanerr10@gmail.com>
2022-04-20 19:27:57 -0300

    EstadoService.java

--- src/main/java/com/app/deliveryapp/domain/service/EstadoService.java
+++ src/main/java/com/app/deliveryapp/domain/service/EstadoService.java
@@ -1,7 +1,6 @@
 package com.app.deliveryapp.domain.service;
 
-import com.app.deliveryapp.domain.exception.EntidadeEmUsoException;
-import com.app.deliveryapp.domain.exception.EntidadeNaoEcontradaException;
+import com.app.deliveryapp.domain.exceptionhandler.EntidadeNaoEncontradaException;
 import com.app.deliveryapp.domain.model.Estado;
 import com.app.deliveryapp.domain.repository.EstadoRepository;
 import lombok.AllArgsConstructor;
@@ -14,21 +13,26 @@ public class EstadoService {
 
     private EstadoRepository estadoRepository;
 
+
+    public static final String NÃO_ECONTRADO = "NÃO ECONTRADO";
+
+    public Estado buscarId(Long id){
+        return estadoRepository.findById(id)
+                .orElseThrow(()-> new EntidadeNaoEncontradaException(NÃO_ECONTRADO));
+    }
+
     public Estado salvar(Estado estado){
         return estadoRepository.save(estado);
     }
 
     public void deletaEstado(Long id){
 
-        if(!estadoRepository.existsById(id)){
-            throw new EntidadeNaoEcontradaException("Esse Estado nao foi localizada no seu banco de dados");
-        }
-
+        buscarId(id);
         try {
             estadoRepository.deleteById(id);
         }
         catch (DataIntegrityViolationException E){
-            throw new   EntidadeEmUsoException("Não foi possivel a deleção, pois esse item se encontra associado " +
+            throw new EntidadeNaoEncontradaException("Não foi possivel a deleção, pois esse item se encontra associado " +
                     "a outra tabela do seu banco de dados");
         }
     }
