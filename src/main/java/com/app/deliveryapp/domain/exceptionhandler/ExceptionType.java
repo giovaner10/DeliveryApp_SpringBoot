09de5228741d59b865f02c4887caff6b9e98a409
giovaner10 <giovanerr10@gmail.com>
2022-04-21 16:59:44 -0300

    Novos tipos de excessoes

--- src/main/java/com/app/deliveryapp/domain/exceptionhandler/ExceptionType.java
+++ src/main/java/com/app/deliveryapp/domain/exceptionhandler/ExceptionType.java
@@ -9,7 +9,14 @@ public enum ExceptionType {
 
     ENTIDADE_EM_USO("Entidades associadas a outras entidades não podem ser deletadas", "/item-associado-a-outro"),
 
-    MENSAGEM_BODY_IMCOMPREEMSIVEL("Mensagem incompreensivel", "/mensgem-imcompreensivel");
+    MENSAGEM_BODY_IMCOMPREEMSIVEL("Mensagem incompreensivel", "/mensgem-imcompreensivel"),
+
+    PARAMETRO_INVALIDO("A url possui um parametro invalido", "/paramentro-invalido"),
+
+    ERRO_NO_SISTEMA("Ocoreu um erro interno, tente novamente, se persistir entre em contato com o administrador", "/error"),
+
+    DADOS_INVALIDOS("Essa operação não foi concluida pois apresenta dados invalidos", "/dados-invalidos");
+
 
     private String title;
     private String uri;
